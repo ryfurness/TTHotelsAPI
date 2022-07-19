d00002f2624d2308dec680eb14b9db445f952e53
ryfurness <ryan.f.allard@gmail.com>
2022-07-19 15:18:15 -0400

    added search function on client and server side

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert('Working')
+console.log('Working')
+document.querySelector('#hot-searchBtn').addEventListener("click", search)
+
+document.querySelector('#hot-searchBar').onkeypress = function(e){
+    if (!e) e = window.event;
+    var keyCode = e.code || e.key;
+    if (keyCode == 'Enter'){
+      // Enter pressed
+	  search()
+      return false;
+    }
+  }
+
+function search(){
+    const searchTerms = document.querySelector('#hot-searchBar').value
+    const result = fetch("/api/"+searchTerms)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data);
+        document.querySelector('#msg').hidden = false
+        document.querySelector('#hTitle').innerText = "Hotel Name: " + data[0].name;
+        document.querySelector('#hAddress').innerText = "Hotel Address: " + data[0].address;
+        document.querySelector('#hCity').innerText = "Hotel City: " + data[0].city;
+        document.querySelector('#hPhone').innerText = "Hotel Phone: " + data[0].phone;
+    })
+    .catch(err=> console.log(`Error: ${err}.`))
+
+}
--- server.js
+++ server.js
@@ -1,39 +1,54 @@
-<<<<<<< HEAD
 const express = require('express')
 const app = express()
-const PORT = 8000
+const fs = require('fs')
+const csv=require('csvtojson');
+const search = require("data-search")
+const dataFile = "./data/tthotels.csv"
+const PORT = 1800
+let hotels = []
+let hotelSearch
 
-const rappers = {
-    '21 savage':{
-        'age': 29,
-        'birthName':'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England' 
-    },
-    'chance the rapper':{
-        'age': 29,
-        'birthName':'Chancelor Bennett',
-        'birthLocation': 'Chicago, Illinois' 
-    },
-    'dylan':{
-        'age': 29,
-        'birthName':'Dylan',
-        'birthLocation': 'Dylan' 
-    }
+function readCSVtoSearch(){
+    csv({delimiter:','})
+    .fromFile(dataFile)
+    .then((json)=>{
+        json.forEach((row,i)=> {
+            hotels.push(row);
+            hotels[i].id = i+1;
+        })
+        processHotels(hotels);
+
+        hotelSearch = search.dataSetGenerate({
+            array: hotels, 
+            nameId: 'id',
+            attributes: ['name','address','city','website'],
+            ignoreInTags:['hotel','resort','guesthouse']
+        })
+        //console.log(hotelSearch)
+    })
 }
+
+function processHotels(hot){
+    console.log(`\nHotels, Guesthouses and Resorts Included: ${hot.length}`)
+    //hotels.forEach((h,i)=> console.log(`${h.id}: ${h.name}`));
+}
+
+readCSVtoSearch()
 
 app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
-
-app.get('/api/:rapperName', (request,response)=>{
-    const rappersName = request.params.rapperName.toLowerCase()
-    if(rappers[rappersName]){
-        response.json(rappers[rappersName])
-    }else{
-        response.json(rappers['dylan'])
-    }
+app.get('/js/main.js', (request, response)=>{
+    response.sendFile(__dirname + '/js/main.js')
+})
+app.get('/api/:hNameSearch', (request,response)=>{
+    const hotelName = request.params.hNameSearch//.toLowerCase()
+    console.log(hotelName)
+    let results = search.search(hotelSearch, hotelName, false)
+    console.log(results.result)
+    response.send(JSON.stringify(results.result))
 })
 
 app.listen(PORT, ()=>{
-    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+    console.log(`The server is running on port ${PORT}!`)
 })
